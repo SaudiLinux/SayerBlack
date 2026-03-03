bac37d8d8b9a03679afc6d75d15f523f7bb492de
Saudi Linux <SaudiLinux@users.noreply.github.com>
2026-03-03 04:36:19 +0300

    Add files via upload

--- db/database.py
+++ db/database.py
@@ -1,1 +1,9 @@
+import sqlite3, json
 
+def save_scan(target, results):
+    conn = sqlite3.connect("sayerblack_pro.db")
+    c = conn.cursor()
+    c.execute("CREATE TABLE IF NOT EXISTS scans (target TEXT, data TEXT)")
+    c.execute("INSERT INTO scans VALUES (?,?)", (target, json.dumps(results)))
+    conn.commit()
+    conn.close()
